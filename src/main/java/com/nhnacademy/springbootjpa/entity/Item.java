1ffbd99329b49fea8991225011bbab1d606282e6
Poly-Etilen <wlsdud04060@naver.com>
2025-11-03 13:43:10 +0900

    NotBlank로 수정

--- src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/Item.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 // TODO #1: `item` 테이블과 매핑될 `Item` Entity 클래스를 작성하세요.
@@ -24,9 +24,9 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+    @NotBlank
     private String name;
 
-    @NotNull
+    @NotBlank
     private long price;
 }

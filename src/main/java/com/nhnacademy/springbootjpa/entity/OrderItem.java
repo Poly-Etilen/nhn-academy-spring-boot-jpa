e0b5f24db6a06e53ec648133cc33fd82592f733c
Poly-Etilen <wlsdud04060@naver.com>
2025-11-05 09:20:28 +0900

    실습완료

--- src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
+++ src/main/java/com/nhnacademy/springbootjpa/entity/OrderItem.java
@@ -1,22 +1,31 @@
 package com.nhnacademy.springbootjpa.entity;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "line_number"))
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class OrderItem {
-    @EmbeddedId
-    private OrderItemPk pk;
+    @Id
+    @Column(name = "order_item_id")
+    private long orderItemId;
 
-    // TODO #1: 다대일 관계 설정
+//    @Column(name = "order_id")
+//    private long orderId;
+    @ManyToOne
+    private Order order;
+
+    @Column(name = "line_number")
+    private int lineNumber;
+
+//    @Column(name = "item_id")
+//    private long itemId;
     @ManyToOne(optional = false)
     private Item item;
 
